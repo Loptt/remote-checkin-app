524720e186ea8e5b8ef5648f595cff2c84258d51
Carlos Estrada <carlos.deg02@hotmail.com>
2018-06-07 11:49:38 -0500

    Added main activity xml and button

--- QualtecCheckIn/app/src/main/java/com/carlos/qualteccheckin/MainActivity.java
+++ QualtecCheckIn/app/src/main/java/com/carlos/qualteccheckin/MainActivity.java
@@ -2,12 +2,25 @@ package com.carlos.qualteccheckin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.main_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }

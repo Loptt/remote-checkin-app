00dd4cfad1d1421f4a5f02c70fa62160bd1ff2af
Carlos Estrada <carlos.deg02@hotmail.com>
2018-06-10 14:39:37 -0500

    Config update

--- QualtecCheckIn/app/src/main/java/com/carlos/qualteccheckin/MailAPI/ConfigMail.java
+++ QualtecCheckIn/app/src/main/java/com/carlos/qualteccheckin/MailAPI/ConfigMail.java
@@ -2,5 +2,5 @@ package com.carlos.qualteccheckin.MailAPI;
 
 public class ConfigMail {
     public static final String EMAIL = "estrada.carlosd@gmail.com";
-    public static final String PASSWORD = "nosemeocurre";
+    public static final String PASSWORD = "";
 }

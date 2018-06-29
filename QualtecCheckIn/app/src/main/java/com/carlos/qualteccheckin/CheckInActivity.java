31c9c2e4aff0d29a3bdb989ebaf4c943c64a402d
Carlos Estrada <carlos.deg02@hotmail.com>
2018-06-28 23:07:07 -0500

    Added mail format in Checkin act

--- QualtecCheckIn/app/src/main/java/com/carlos/qualteccheckin/CheckInActivity.java
+++ QualtecCheckIn/app/src/main/java/com/carlos/qualteccheckin/CheckInActivity.java
@@ -1,5 +1,9 @@
 package com.carlos.qualteccheckin;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.location.Location;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,17 +11,25 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.security.PrivateKey;
 import java.util.Properties;
+import java.util.Calendar;
+import java.util.concurrent.CancellationException;
 
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 
 import com.carlos.qualteccheckin.MailAPI.SendMail;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class CheckInActivity extends AppCompatActivity{
+
+    private final int CHECKING_IN = 1;
+    private final int CHEKING_OUT = 2;
 
     private Button enterButton;
     private Button exitButton;
@@ -28,6 +40,9 @@ public class CheckInActivity extends AppCompatActivity{
     private String username, email;
 
     private FusedLocationProviderClient locationProviderClient;
+    private Calendar date;
+
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +55,8 @@ public class CheckInActivity extends AppCompatActivity{
         usernameTextView = (TextView) findViewById(R.id.check_in_username);
 
         user = FirebaseAuth.getInstance().getCurrentUser();
+
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         if (user != null) {
             username = user.getDisplayName();
@@ -58,7 +75,7 @@ public class CheckInActivity extends AppCompatActivity{
             public void onClick(View view) {
                 if (user != null) {
                     Toast.makeText(CheckInActivity.this, "Entrada", Toast.LENGTH_SHORT).show();
-                    sendEmail("Entrando");
+                    checkIn(CHECKING_IN);
                 }
                 else {
                     //User not correctly authenticated
@@ -72,7 +89,7 @@ public class CheckInActivity extends AppCompatActivity{
             public void onClick(View view) {
                 if (user != null) {
                     Toast.makeText(CheckInActivity.this, "Salida", Toast.LENGTH_SHORT).show();
-                    sendEmail("Saliendo");
+                    checkIn(CHEKING_OUT);
                 }
                 else {
                     //User not correctly authenticated
@@ -80,6 +97,59 @@ public class CheckInActivity extends AppCompatActivity{
                 }
             }
         });
+    }
+
+    private void checkIn(final int status) {
+
+        int result = PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        progressDialog = new ProgressDialog(CheckInActivity.this);
+        progressDialog.setMessage("Solicitud en progreso");
+        progressDialog.show();
+
+        if (result != PermissionChecker.PERMISSION_DENIED) {
+            locationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+
+                    if (location != null) {
+                        date = Calendar.getInstance();
+
+                        prepareAndSendEmail(status, date, location);
+                    }
+                    else {
+                        Toast.makeText(CheckInActivity.this, "Error de Checkin", Toast.LENGTH_SHORT).show();
+                    }
+
+                    progressDialog.dismiss();
+                }
+            });
+        }
+    }
+
+    private void prepareAndSendEmail(int status, Calendar date, Location location) {
+        String message;
+
+        String year = String.valueOf(date.get(Calendar.YEAR));
+        String month = String.valueOf(date.get(Calendar.MONTH));
+        String day = String.valueOf(date.get(Calendar.DAY_OF_MONTH));
+
+        String hour = String.valueOf(date.get(Calendar.HOUR));
+        String minutes = String.valueOf(date.get(Calendar.MINUTE));
+
+        String latitude = String.valueOf(location.getLatitude());
+        String longitude = String.valueOf(location.getLongitude());
+
+        if (status == CHECKING_IN) {
+            message = "Check in \n" + "Date: " + day + "/" + month + "/" + year + " \n" + "Time: " + hour + ":" + minutes
+                    + " \n" + "Latitude: " + latitude + " \n" + "Longitude: " + longitude + "\n";
+        }
+        else {
+            message = "Check out \n" + "Date: " + day + "/" + month + "/" + year + " \n" + "Time: " + hour + ":" + minutes
+                    + " \n" + "Latitude: " + latitude + " \n" + "Longitude: " + longitude + "\n";
+        }
+
+        sendEmail(message);
     }
 
     private void sendEmail(String message) {
